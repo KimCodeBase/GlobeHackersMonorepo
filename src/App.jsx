80f3f4144a5f783065679f6bbd5f51ab28e0ecf5
NoriesDev <noriesdevelopment@gmail.com>
2023-10-06 16:13:06 +0200

    fetching data from contentful but missing the image

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
 import SmCard from './components/SmCard';
 import Navbar from './components/Navbar';
 import './App.css'
+import { useState, useEffect } from 'react';
+import { getBlogPostData } from './components/lib/contentfulClient';
+
 
 
 export default function App() {
+  const [blogPost, setBlogPost] = useState([]);
+
+  useEffect(() => {
+    getBlogPostData();
+    
+    const fetchData = async() => {
+      try {
+        const getBlogPostData = await fetch('https://cdn.contentful.com/spaces/vg79p7oq5l6k/environments/master/entries?access_token=yDB-E814LS6R2KZiL3NwEUIJ9muWipQqpYd50abWBlk&content_type=blogPost&select=fields.title,fields.date,fields.author,fields.article');
+        if (!getBlogPostData.ok) {
+        throw new Error(
+          'the request failed with a status of' + getBlogPostData.status
+        );}
+        const parsedBlogPost = await getBlogPostData.json();
+        console.log(parsedBlogPost);
+        const blogPosts = parsedBlogPost.items || [];
+        setBlogPost(blogPosts);
+      } catch(error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
     <Navbar />
-    <SmCard />
+    {blogPost.map((post, index) => (
+      <SmCard
+        key={index}
+        title={post.fields.title}
+        date={post.fields.date}
+        author={post.fields.author}
+        
+        article={post.fields.article}
+      />
+    ))}
     </>
-  )
-}
-
-
+  );
+  }
+ 
