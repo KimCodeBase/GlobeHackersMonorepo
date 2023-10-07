d4d78abf59c23812231afa443275efe5655ad6c2
NoriesDev <noriesdevelopment@gmail.com>
2023-10-07 17:59:18 +0200

    fetching data and showing it on screen with imagesgit statusgit status

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,37 @@ import { getBlogPostData } from './components/lib/contentfulClient';
 
 
 export default function App() {
-  const [blogPost, setBlogPost] = useState([]);
-
+  const [blogPosts, setBlogPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    getBlogPostData();
-    
-    const fetchData = async() => {
+    const fetchData = async () => {
       try {
-        const getBlogPostData = await fetch('https://cdn.contentful.com/spaces/vg79p7oq5l6k/environments/master/entries?access_token=yDB-E814LS6R2KZiL3NwEUIJ9muWipQqpYd50abWBlk&content_type=blogPost&select=fields.title,fields.date,fields.author,fields.article');
-        if (!getBlogPostData.ok) {
-        throw new Error(
-          'the request failed with a status of' + getBlogPostData.status
-        );}
-        const parsedBlogPost = await getBlogPostData.json();
-        console.log(parsedBlogPost);
-        const blogPosts = parsedBlogPost.items || [];
-        setBlogPost(blogPosts);
+        const blogPostData = await getBlogPostData();
+        setBlogPosts(blogPostData.items);
+        setLoading(false);
       } catch(error) {
         console.error(error);
+        setLoading(false);
       }
     };
     fetchData();
   }, []);
-
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
     <Navbar />
-    {blogPost.map((post, index) => (
-      <SmCard
-        key={index}
-        title={post.fields.title}
-        date={post.fields.date}
-        author={post.fields.author}
-        
-        article={post.fields.article}
-      />
-    ))}
+    {blogPosts.map((post, index) => (
+        <SmCard
+          key={index}
+          title={post.fields.title}
+          date={post.fields.date}
+          author={post.fields.author}
+          photo={post.fields.photo.fields.file.url}
+          article={post.fields.article}
+        />
+      ))}
     </>
   );
   }
