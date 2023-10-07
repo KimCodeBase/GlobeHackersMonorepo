f8409803b5bc75d0f9a23f34905fdb36e5773cae
NoriesDev <noriesdevelopment@gmail.com>
2023-10-07 19:53:56 +0200

    adding backgroudn color

--- src/components/SmCard.jsx
+++ src/components/SmCard.jsx
@@ -7,9 +7,6 @@ export default function SmCard( {title,date, author, photo , description,  artic
 return (
   <div className='outer-div'>
   <div className="container">
-
-
-
     <img className="img" src={photo} alt={description} />
     <div className='rightCard'>
     <h3 className="title">{title}</h3>
