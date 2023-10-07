993be47df35d41b39907f7d1d945f43485ba8ea8
NoriesDev <noriesdevelopment@gmail.com>
2023-10-07 17:59:18 +0200

    fetching data and showing it on screen with imagesgit statusgit status

--- src/components/SmCard.jsx
+++ src/components/SmCard.jsx
@@ -2,29 +2,29 @@ import './SmCardStyling.css';
 import PropTypes from 'prop-types';
 
 
-export default function SmCard( {title,date, author, photo, article} ) {
+export default function SmCard( {title,date, author, photo , article} ) {
 
 return (
-<div className='outer-div'>
+
   <div className="container">
     <h3 className="title">{title}</h3>
     <div className='card-header'>
     <p className="date">{date}</p>
     <p className="author">{author}</p>
     </div>
-    <img className="img" src={photo} alt="" />
+    <img className="img" src={photo} alt='Picture' />
     <div className='article'>{article}</div>
     <button className='drop-btn'>Article</button>
   </div>
-</div>
+
   
 )
 }
 
 SmCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  photo: PropTypes.string.isRequired,
-  article: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  date: PropTypes.string,
+  author: PropTypes.string,
+  photo: PropTypes.object,
+  article: PropTypes.string,
 };
