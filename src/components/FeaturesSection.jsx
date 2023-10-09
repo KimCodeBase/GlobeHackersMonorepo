ec909426d1183d111a07fc913eaebd0cf7fb6cf2
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-10-09 12:33:05 +0200

    footer,header

---
+++ src/components/FeaturesSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function FeaturesSection() {
+  return (
+    <div>
+      <h2>Bucket List</h2>
+      {/* Other content */}
+    </div>
+  );
+}
+
+export default FeaturesSection;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeaderStyling.css";
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <h2 className="header-title">Explore the World with GlobeHackers</h2>
+      <p className="header-subtitle">
+        Discover new places, cultures, and adventures.
+      </p>
+    </div>
+  );
+}
