b8c41deadc808089e556a28e6157c829bec8a180
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-10-09 23:44:11 +0200

    updated blog

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,13 @@ import "./HeaderStyling.css";
 export default function Header() {
   return (
     <div className="header-container">
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search articles..."
+          className="search-input"
+        />
+      </div>
       <h2 className="header-title">Explore the World with GlobeHackers</h2>
       <p className="header-subtitle">
         Discover new places, cultures, and adventures.
