ae6d345731ba8c6b98305663ee9f01770aff24b4
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-10-09 12:33:05 +0200

    footer,header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import SmCard from "./components/SmCard";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import FeaturesSection from "./components/FeaturesSection";
 import "./App.css";
 import Footer from "./components/Footer";
 
@@ -7,6 +9,8 @@ export default function App() {
   return (
     <>
       <Navbar />
+      <Header />
+      <FeaturesSection />
       <SmCard />
       <Footer />
     </>
