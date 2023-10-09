a7c33cedc7a6e6caf5ecfdebbf3ec48c443b307a
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-10-09 14:18:22 +0200

    changes blog frontend

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./FeaturesSection.css";
 
 function FeaturesSection() {
   return (
-    <div>
-      <h2>Bucket List</h2>
-      {/* Other content */}
+    <div className="features-section">
+      <h1 className="title">GLOBEHACKERS</h1>
+      <p className="subtitle">Embark on journeys, discover untold stories.</p>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,34 @@
+import React from "react";
 import "./FooterStyling.css";
 
 export default function Footer() {
   return (
     <div className="footer-container">
       <div className="footer-content">
-        <h3 className="footer-logo">GLOBEHACKERS</h3>
-        <ul className="footer-menu">
-          <li className="footer-menu-items">About Us</li>
-          <li className="footer-menu-items">Contact</li>
-          <li className="footer-menu-items">Terms & Conditions</li>
-        </ul>
-        <p className="footer-text">© 2023 GlobeHackers. All rights reserved.</p>
+        <div className="newsletter-section">
+          <input
+            type="email"
+            placeholder="Your email address"
+            className="newsletter-input"
+          />
+          <button className="newsletter-btn">Subscribe</button>
+        </div>
+        <div className="footer-right">
+          <div className="social-links">
+            <a href="#">Instagram</a>
+            <a href="#">Facebook</a>
+            <a href="#">Twitter</a>
+            <a href="#">Pinterest</a>
+          </div>
+          <div className="footer-links">
+            <a href="#">About</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Impressum</a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        © 2023 GLOBEHACKERS. All rights reserved.
       </div>
     </div>
   );
