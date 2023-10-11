5d75984a8f529938ecb7c77b24d54e23c6d9ca82
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-10-11 21:28:35 +0200

    this is not an update, im just commiting so that i can pull the main updates

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -4,16 +4,26 @@ import "./FeaturesSection.css";
 function FeaturesSection() {
   return (
     <div className="features-section">
-      <h1 className="title">GLOBEHACKERS</h1>
-      <p className="subtitle">
-        At GLOBEHACKERS, we believe in the transformative power of travel. Our
-        mission is to inspire individuals to step out of their comfort zones,
-        explore the unknown, and connect with diverse cultures around the world.
-        Every journey tells a story, and we're here to share those tales of
-        adventure, discovery, and human connection. Join us as we traverse the
-        globe, uncover hidden gems, and celebrate the spirit of wanderlust.
-      </p>
-      <button className="explore-btn">EXPLORE</button>
+      <div className="content-wrapper">
+        <img
+          src="public/juli-kosolapova-be5B3YGDmtc-unsplash.jpg"
+          alt="GLOBEHACKERS Feature"
+          className="feature-image"
+        />
+        <div className="text-content">
+          <h1 className="title">GLOBEHACKERS</h1>
+          <p className="subtitle">
+            At GLOBEHACKERS, we believe in the transformative power of travel.
+            Our mission is to inspire individuals to step out of their comfort
+            zones, explore the unknown, and connect with diverse cultures around
+            the world. Every journey tells a story, and we're here to share
+            those tales of adventure, discovery, and human connection. Join us
+            as we traverse the globe, uncover hidden gems, and celebrate the
+            spirit of wanderlust.
+          </p>
+          <button className="explore-btn">EXPLORE</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/LgCard.jsx
+++ src/components/LgCard.jsx
@@ -1,41 +1,43 @@
-import './Lg-Card-styling.css'
-import PropTypes from 'prop-types';
+import "./Lg-Card-styling.css";
+import PropTypes from "prop-types";
 
-export default function LgCard ( {title, date, author,  photo, description, article, listTitle,  recommendations}) {
-
-    return ( 
-<div className="Lg-container">
-<div className='Lg-rightCard'>
-    <h3 className="Lg-title"> {title} </h3>
-    <div className='Lg-float'>
-    <p className="Lg-author"> {author} </p>
-    <p className="Lg-date"> {date} </p>
-    </div>
-    <div className='Lg-article'> {article} </div>
-    </div>
-    <div className='Lg-Top-Section'>
-    <img className="Lg-img" src= {photo} alt={description} />
-        <ul className='LgList'>
-            <h2 className='li-title'> { listTitle } </h2> 
-        <li className='li-items'> {recommendations[0]} </li>
-        <li className='li-items'> {recommendations[1]} </li>
-        <li className='li-items'> {recommendations[2]} </li>
-        <li className='li-items'> {recommendations[3]} </li>
-        <li className='li-items'> {recommendations[4]} </li>
-        </ul>
+export default function LgCard({
+  title,
+  date,
+  author,
+  photo,
+  description,
+  article,
+  listTitle,
+  recommendations,
+}) {
+  return (
+    <div className="Lg-container">
+      <div className="Lg-rightCard">
+        <h3 className="Lg-title"> {title} </h3>
+        <div className="Lg-float">
+          <p className="Lg-author"> {author} </p>
+          <p className="Lg-date"> {date} </p>
         </div>
-  </div>
-)
+        <div className="Lg-article"> {article} </div>
+      </div>
+      <div className="Lg-Top-Section">
+        <img className="Lg-img" src={photo} alt={description} />
+        <ul className="LgList">
+          <h2 className="li-title"> {listTitle} </h2>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-
 LgCard.propTypes = {
-    title: PropTypes.string,
-    date: PropTypes.string,
-    author: PropTypes.string,
-    photo: PropTypes.object,
-    description: PropTypes.string,
-    article: PropTypes.string,
-    listTitle:PropTypes.string,
-    recommendations: PropTypes.array
-  };
+  title: PropTypes.string,
+  date: PropTypes.string,
+  author: PropTypes.string,
+  photo: PropTypes.object,
+  description: PropTypes.string,
+  article: PropTypes.string,
+  listTitle: PropTypes.string,
+  recommendations: PropTypes.array,
+};
