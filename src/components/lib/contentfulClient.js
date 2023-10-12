d9697e26bc5f3e63238079899fe7f9b736865825
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-10-12 22:48:09 +0200

    updates images deleted

--- src/components/lib/contentfulClient.js
+++ src/components/lib/contentfulClient.js
@@ -1,5 +1,5 @@
-import { createClient } from 'contentful';
-import { createClient as managementClient } from 'contentful-management'
+import { createClient } from "contentful";
+import { createClient as managementClient } from "contentful-management";
 
 const client = createClient({
   space: import.meta.env.VITE_SPACE_ID,
@@ -13,36 +13,39 @@ const mngClient = managementClient({
 const getBlogPostData = async () => {
   try {
     const getBlogPostEntries = await client.getEntries({
-      content_type: 'blogPost',
+      content_type: "blogPost",
       include: 10,
     });
     console.log(getBlogPostEntries);
     return getBlogPostEntries;
-  } catch(error) {
+  } catch (error) {
     console.error(error.message);
-    }
+  }
 };
 
 const createBlogPostEntry = async (formData) => {
   try {
-    mngClient.getSpace(import.meta.env.VITE_SPACE_ID)
-.then((space) => space.getEnvironment('master'))
-.then((environment) => environment.createEntry('blogPost', {
-  fields: {
-    title: {'en-US': formData.title},
-        date: {'en-US': formData.date},
-        author: {'en-US': formData.author},
-        article: {'en-US': formData.article},
-        imageUrl:  {'en-US': formData.imageUrl},
-        dining: {'en-US':formData.dining},
-        adventures: {'en-US':formData.adventures},
-        shopping: {'en-US':formData.shopping}
-  }
-}))
-.then((entry) => entry.publish().then(console.log(entry)))
-  } catch(error) {
+    mngClient
+      .getSpace(import.meta.env.VITE_SPACE_ID)
+      .then((space) => space.getEnvironment("master"))
+      .then((environment) =>
+        environment.createEntry("blogPost", {
+          fields: {
+            title: { "en-US": formData.title },
+            date: { "en-US": formData.date },
+            author: { "en-US": formData.author },
+            article: { "en-US": formData.article },
+            imageUrl: { "en-US": formData.imageUrl },
+            dining: { "en-US": formData.dining },
+            adventures: { "en-US": formData.adventures },
+            shopping: { "en-US": formData.shopping },
+          },
+        })
+      )
+      .then((entry) => entry.publish().then(console.log(entry)));
+  } catch (error) {
     console.error(error.message);
-    }
-}
+  }
+};
 
 export { getBlogPostData, createBlogPostEntry };
