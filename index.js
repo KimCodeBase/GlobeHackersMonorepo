26ed973a1d617afe50109447375efe2b2eb4a50a
NoriesDev <noriesdevelopment@gmail.com>
2023-10-23 17:52:37 +0200

    not winning with fetching the array from blogPost, will try fix tomorrow

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const server = express();
 const port = process.env.PORT || 8000;
+const blogPost = require('./blogPost');
 
-
+server.use(express.json());
 server.get('/', (req, res) =>
   res.send(
     `<p>server is listening on port ${port}</p>`
@@ -11,8 +12,22 @@ server.get('/', (req, res) =>
 
 
 server
-.route('/')
-.get()
+.route('/home')
+.get((req, res) => {
+const { title, date, author, imageUrl, article, dining, adventure, shopping } = req.params;
+
+res.send(`
+    ${blogPost.map((index) => `
+    <h1>${title}</h1>
+    <h3>${date}/${author}</h3>
+    <img src="${imageUrl}" />
+    <h5>${article}</h5>
+    <p>${dining}</p>
+    <p>${adventure}</p>
+    <p>${shopping}</p>
+    `).join('')}
+`)
+})
 
 
 server
